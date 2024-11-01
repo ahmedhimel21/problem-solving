1bb523663bc7824c3e40435c532296524456de13
Ahmed Himel <ahmedhimel000@gmail.com>
2024-11-02 00:24:20 +0600

    array-filtering-and-mapping

---
+++ array-filtering-and-mapping.js
@@ -0,0 +1,15 @@
+const persons = [
+  { name: "Tanvir", age: 25, gender: "female" },
+  { name: "Sabbir", age: 30, gender: "male" },
+  { name: "Johny", age: 22, gender: "male" },
+  { name: "Afrin", age: 27, gender: "female" },
+];
+
+const getMaleNames = (personsArr) => {
+  const personName = personsArr
+    .filter((person) => person.gender === "male")
+    .map((person) => person.name);
+  return personName;
+};
+
+console.log(getMaleNames(persons));
